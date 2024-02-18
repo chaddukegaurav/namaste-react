ce058212fa6afb5f54d5a03993d4177f13fcfa21
Gaurav <gaurav-mohan.chadduke@capgemini.com>
2024-02-18 17:41:37 +0530

    epi-01

---
+++ App.js
@@ -0,0 +1,34 @@
+
+
+  /* 
+   C A S E 1
+  <div id="Parent">
+    <div id="Child">
+        <h1>Hello World from H1</h1>
+    </div>
+</div> 
+
+  C A S E 2
+
+   <div id="Parent">
+    <div id="Child">
+        <h1>Hello World from H1</h1>
+        <h2>Hello World from H1</h2>
+    </div>
+</div> 
+
+*/
+
+
+const xyz = React.createElement(
+  "div",
+  { id: "Parent" },
+  React.createElement(
+    "div",
+    { id: "Child" },
+    [React.createElement("h1", {}, "Hello World from H1"),React.createElement("h2", {}, "Hello World from H2 ")]
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(xyz);
